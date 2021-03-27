c93552a63cc844ed6c7459a2a0976b90f26c21a9
dreamcmi <1912544842@qq.com>
2021-03-27 13:55:06 +0800

    add Air724 demo

---
+++ src/air724/main.lua
@@ -0,0 +1,45 @@
+--必须在这个位置定义PROJECT和VERSION变量
+--PROJECT：ascii string类型，可以随便定义，只要不使用,就行
+--VERSION：ascii string类型，如果使用Luat物联云平台固件升级的功能，必须按照"X.X.X"定义，X表示1位数字；否则可随便定义
+-- MODEL = "Air202"
+--PRODUCT_KEY = "FlAQcpoFXH7EpYfD5Ao289iUBQT5Y30H"
+PROJECT = "RC522Test"
+VERSION = "1.0.0"
+
+--加载日志功能模块，并且设置日志输出等级
+--如果关闭调用log模块接口输出的日志，等级设置为log.LOG_SILENT即可
+require "log"
+LOG_LEVEL = log.LOGLEVEL_TRACE
+--[[
+如果使用UART输出日志，打开这行注释的代码"--log.openTrace(true,1,115200)"即可，根据自己的需求修改此接口的参数
+如果要彻底关闭脚本中的输出日志（包括调用log模块接口和Lua标准print接口输出的日志），执行log.openTrace(false,第二个参数跟调用openTrace接口打开日志的第二个参数相同)，例如：
+1、没有调用过sys.opntrace配置日志输出端口或者最后一次是调用log.openTrace(true,nil,921600)配置日志输出端口，此时要关闭输出日志，直接调用log.openTrace(false)即可
+2、最后一次是调用log.openTrace(true,1,115200)配置日志输出端口，此时要关闭输出日志，直接调用log.openTrace(false,1)即可
+]]
+--log.openTrace(true,1,115200)
+
+require "sys"
+require "pins"
+require "net"
+--每1分钟查询一次GSM信号强度
+--每1分钟查询一次基站信息
+net.startQueryAll(60000, 60000)
+
+-- require "wdt"
+-- wdt.setup(pio.P0_30, pio.P0_31)
+
+
+--加载网络指示灯功能模块
+--根据自己的项目需求和硬件配置决定：1、是否加载此功能模块；2、配置指示灯引脚
+--合宙官方出售的Air800和Air801开发板上的指示灯引脚为pio.P0_28，其他开发板上的指示灯引脚为pio.P1_1
+--require "netLed"
+--netLed.setup(true,pio.P0_28)
+--网络指示灯功能模块中，默认配置了各种工作状态下指示灯的闪烁规律，参考netLed.lua中ledBlinkTime配置的默认值
+--如果默认值满足不了需求，此处调用netLed.updateBlinkTime去配置闪烁时长
+
+require "RC522"
+require "test"
+
+--启动系统框架
+sys.init(0, 0)
+sys.run()
---
+++ src/air724/test.lua
@@ -0,0 +1,92 @@
+----------------------------------------------------------------------
+-- Main
+----------------------------------------------------------------------
+module(...,package.seeall)
+
+--------------------------------------------------------
+--  Converts a table of numbers into a HEX string
+function appendHex(t)
+  strT = ""
+  for i,v in ipairs(t) do
+    strT = strT.." 0x"..string.format("%X", t[i])
+  end
+  return strT
+end
+
+---------------------------------------------------
+-- Initialise the RC522
+
+
+sys.taskInit(function()
+  sys.wait(3000)
+  RC522.setup()
+  sys.wait(100)
+  print("=================================================")
+  print("RC522 Firmware Version: 0x"..string.format("%X", RC522.getFirmwareVersion()))
+
+  while true do
+    sys.wait(1000)
+    isTagNear, cardType = RC522.request()
+    
+    if isTagNear == true then
+      --tmr.stop(0)
+      err, serialNo = RC522.anticoll()
+      print("Tag Found: "..appendHex(serialNo).."  of type: "..appendHex(cardType))
+
+      -- Selecting a tag, and the rest afterwards is only required if you want to read or write data to the card
+    
+      err, sak = RC522.select_tag(serialNo)
+      if err == false then
+        print("Tag selected successfully.  SAK: 0x"..string.format("%X", sak))
+    
+    
+        for i = 0,63 do
+          err = RC522.card_auth(auth_a, i, keyA, serialNo)     --  Auth the "A" key.  If this fails you can also auth the "B" key
+          if err then 
+            print("ERROR Authenticating block "..i)
+          else
+            -- Write data to card, enable if you need it
+            --if (i == 2) then   -- write to block 2
+            --  err = RC522.writeTag(i, { 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 })
+            --  if err then print("ERROR Writing to the Tag") end
+            --end
+    
+            -- Read card data
+            if not (i % 4 == 3) then   --  Don't bother to read the Sector Trailers 
+              err, tagData = RC522.readTag(i)
+              if not err then print("READ Block "..i..": "..appendHex(tagData)) end
+            end
+          end
+        end
+    
+        
+      else
+        print("ERROR Selecting tag")
+    
+      end
+      print(" ")
+    
+      -- halt tag and get ready to read another.
+      buf = {}
+      buf[1] = 0x50  --MF1_HALT
+      buf[2] = 0
+      crc = RC522.calculate_crc(buf)
+      table.insert(buf, crc[1])
+      table.insert(buf, crc[2])
+      err, back_data, back_length = RC522.card_write(mode_transrec, buf)
+      RC522.clear_bitmask(0x08, 0x08)    -- Turn off encryption
+      
+      --tmr.start(0)
+      
+    else 
+      print("NO TAG FOUND")
+    end
+  end
+end)
+
+--[[
+tmr.alarm(0, 100, tmr.ALARM_AUTO, function()
+
+
+end)  -- timer
+]]
